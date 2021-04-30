fb424db163f72493315fb16a49eba01b1f6c6632
dani <dan.yaniv@bitmovin.com>
2021-04-29 21:54:34 -0400

    zd7929 - processed vtt

--- test1.js
+++ test1.js
@@ -3,9 +3,41 @@ const config = {
   logs: {
       level: "debug"
   },
-  "playback": {
-    "preferredTech":
-      [{"player": "native", "streaming": "hls"}]
+//  "playback": {
+//    "preferredTech": [{"player":"html5","streaming":"hls"}]
+//  },
+  network: {
+    preprocessHttpResponse:
+      function(type, response)
+      {
+        console.log("**** ON HTTP RESPONSE ***", type);
+        if (type == "media/subtitles") {
+          let originalVtt;
+          if (player.getPlayerType() == "html5") {
+            let data = new DataView(response.body);
+            let decoder = new TextDecoder("utf-8");
+            console.log("***** ON SUBTITLE REQUEST ****");
+            originalVtt = decoder.decode(data);
+          }
+          else originalVtt = response.body;
+
+          let vttProcessed = originalVtt
+            .split(/\n([0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{3} --> [0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{3}[^\n]*)\n/)
+            .map(
+              (el) =>
+                el.replaceAll( /(.)\n(.)/g, "$1<br/>$2" )
+            )
+            .join("\n");
+
+          if (player.getPlayerType() == "html5") {
+            let textEncoder = new TextEncoder("utf-8");
+            let dataProcessed = textEncoder.encode(vttProcessed);
+            response.body = dataProcessed.buffer;
+          }
+          else response.body = vttProcessed;
+      }
+      return response;
+    }
   },
   analytics: {
     key: 'ec4c99e5-1874-4f23-bfae-19fb4ef9a0e8',
@@ -20,32 +52,21 @@ const config = {
     customData5: 'custom data'
   }
 };
-const container = document.getElementById('my-player');
+const container = document.getElementById('player-test1');
 const player = new bitmovin.player.Player(container, config);
 
-
 const source = {
-  "hls": "https://bitmovin-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8",
-  subtitleTracks: [{
-    "url": "https://bitmovin-amer-public.s3.amazonaws.com/internal/dani/pw-3335/pw-3335-positioning-1.vtt",
-    id: "id1",
-    label: "css-positioning-1",
-    lang: "en",
-    enabled: false,
-  }, {
-    "url": "https://bitmovin-amer-public.s3.amazonaws.com/internal/dani/pw-3335/pw-3335-spacing-1.vtt",
-    id: "id2",
-    label: "css-spacing-1",
-    lang: "en",
-    enabled: false,
-  }],
+    "hls": "https://bitmovin-amer-public.s3.amazonaws.com/internal/dani/Wed_Apr_21_17%3A48%3A00_EDT_2021/zd7929-test1.m3u8",
+    "subtitleTracks": [{url: "https://bitmovin-amer-public.s3.amazonaws.com/internal/dani/Wed_Apr_21_17%3A48%3A00_EDT_2021/zd7929-positioning-1.vtt", id:"positioning-external-id", label:"positioning-external", lang:"es"}],
   };
 player.load(source).then(
-  function () {
+  function()
+  {
     for (ii of player.subtitles.list()) console.log(ii);
     console.log('[info] player.load() resolved - success');
   },
-  function (reason) {
+  function (reason)
+  {
     console.log('[!] player.load() resolved - fail');
   }
 );
